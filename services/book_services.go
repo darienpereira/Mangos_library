1be52f964683d0515205c43f0431aaaead650086
Immy535 <imranchaudhury18@gmail.com>
2025-07-15 19:34:45 +0100

    add listMyBook func

--- services/book_services.go
+++ services/book_services.go
@@ -1,7 +1,25 @@
 package services
 
-import "library/repository"
+import (
+	"errors"
+	"library/models"
+	"library/repository"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type BookService struct {
 	Repo repository.BookRepository
+}
+
+func (s *BookService) ListBookByUserID(books *[]models.Book, claims jwt.MapClaims) error{
+	userId, ok := claims["userID"].(string)
+	if !ok {
+		return errors.New("no userid in claims")
+	}
+	books, err := s.Repo.GetBooksByUser(userId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
