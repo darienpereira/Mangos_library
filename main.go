e4b6ed418f1119605821a2c22dbc39c09eeffb88
Immy535 <imranchaudhury18@gmail.com>
2025-07-15 17:47:35 +0100

    set up repo, service and handler layers

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"library/database"
+	"library/handlers"
+	"library/repository"
+	"library/routes"
+	"library/services"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
-	
+	/* initialise database */
+	database.OpenDb()
+
+	/* initialise repositories */
+	userRepo := &repository.UserRepo{}
+	bookRepo := &repository.BookRepo{}
+
+	/* initialise service */
+	userService := &services.UserService{Repo: userRepo}
+	bookService := &services.BookService{Repo: bookRepo}
+
+	/* initialise handlers */
+	userHandler := &handlers.UserHandler{Service: userService}
+	bookHandler := &handlers.BookHandler{Service: bookService}
+
+	/* initialise routes */
+	r := routes.SetUpRouter(userHandler, bookHandler)
+
+	/* start server */
+	fmt.Println("starting server...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
+	if err != nil {
+		log.Fatal("failed to start server:", err)
+	}
 }
--- routes/router.go
+++ routes/router.go
@@ -1,1 +1,24 @@
 package routes
+
+import (
+	"library/handlers"
+	"library/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+func SetUpRouter(userHandler *handlers.UserHandler, bookHandler *handlers.BookHandler) *mux.Router {
+	r := mux.NewRouter()
+	/* public routes */
+	
+
+	p := r.PathPrefix("/").Subrouter()
+	p.Use(middleware.AuthMiddleware)
+
+	/* user routes */
+
+
+	/* admin routes */
+
+	return r
+}
