ecdb2680435fd0d64e9c9da4e245fd7563c1e957
egizianaa <nourhan.mousa@hotmail.co.uk>
2025-07-16 00:51:19 +0300

    added register & login

--- repository/user_repo.go
+++ repository/user_repo.go
@@ -1,9 +1,36 @@
 package repository
 
+import (
+	"library/database"
+	"library/models"
+)
+
 type UserRepository interface {
+	GetUserByEmail(email string) (*models.User, error)
+	CreateUser(user *models.User) error
 
 }
 
 type UserRepo struct {
 	
+}
+
+func (r *UserRepo) GetUserByEmail (email string) (*models.User, error) {
+	// check if user exists in db
+	var user models.User
+	err := database.Db.Where("email = ?", email).First(&user).Error
+	if err == nil {
+		return &models.User{}, err
+}
+
+return &user, nil
+
+}
+
+func (r *UserRepo) CreateUser(user *models.User) error {
+	err := database.Db.Create(&user).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
