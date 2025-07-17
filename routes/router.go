ef7993219f947f047c24beb08038ae633e401e04
Valencia Maria Monzele <valenciamariamonzele@Valencias-MacBook-Air.local>
2025-07-17 20:01:07 +0100

    Add protected search routes for genre, title, author, and year

--- routes/router.go
+++ routes/router.go
@@ -13,7 +13,6 @@ func SetUpRouter(userHandler *handlers.UserHandler, bookHandler *handlers.BookHa
 	/* public routes */
 	r.HandleFunc("/login", userHandler.Login).Methods("POST")
 	r.HandleFunc("/register", userHandler.RegisterUser).Methods("POST")
-	
 
 	p := r.PathPrefix("/").Subrouter()
 	p.Use(middleware.AuthMiddleware)
@@ -21,8 +20,12 @@ func SetUpRouter(userHandler *handlers.UserHandler, bookHandler *handlers.BookHa
 	/* user routes */
 	p.HandleFunc("/mybooks", bookHandler.ListUserBooks).Methods("GET")
 
+	p.HandleFunc("/search/genre", bookHandler.FindByGenre).Methods("POST")
+	p.HandleFunc("/search/title", bookHandler.FindByTitle).Methods("POST")
+	p.HandleFunc("/search/author", bookHandler.FindByAuthor).Methods("POST")
+	p.HandleFunc("/search/year", bookHandler.FindByYear).Methods("POST")
+
 	/* admin routes */
 
 	return r
 }
-
